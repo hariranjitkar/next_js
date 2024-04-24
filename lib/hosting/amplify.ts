6446d27f3dfda92a8c9b9ab8e452fa4a62bd7972
hariranjitkar <156306890+hariranjitkar@users.noreply.github.com>
2024-04-24 13:49:58 +0200

    update

---
+++ lib/hosting/amplify.ts
@@ -0,0 +1,67 @@
+import { SecretValue } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as amplify from '@aws-cdk/aws-amplify-alpha'
+import { BuildSpec } from 'aws-cdk-lib/aws-codebuild'
+
+type AmplifyHostingProps = {
+	appName: "hosting-fullstack-cdk"
+	branch: "master"
+	ghOwner: "hariranjit"
+	repo: string
+	ghTokenName: "github-token-ex"
+}
+
+export function createAmplifyHosting(
+	scope: Construct,
+	props: AmplifyHostingProps
+) {
+	const amplifyApp = new amplify.App(scope, `${props.appName}-hosting`, {
+		appName: `${props.appName}`,
+		sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+			owner: props.ghOwner,
+			repository: props.repo,
+			oauthToken: SecretValue.secretsManager(props.ghTokenName),
+		}),
+		platform: amplify.Platform.WEB_COMPUTE,
+		autoBranchDeletion: true,
+		customRules: [
+			{
+				source: '/<*>',
+				target: '/index.html',
+				status: amplify.RedirectStatus.NOT_FOUND_REWRITE,
+			},
+		],
+		environmentVariables: {
+			myAmplifyEnv: 'frontend', //process.env.myAmplifyEnv
+		},
+		buildSpec: BuildSpec.fromObjectToYaml({
+			version: 1,
+			frontend: {
+				phases: {
+					preBuild: {
+						commands: ['npm ci'],
+					},
+					build: {
+						commands: ['npm run build'],
+					},
+				},
+				artifacts: {
+					baseDirectory: '.next',
+					files: ['**/*'],
+				},
+				cache: {
+					paths: ['node_modules/**/*'],
+				},
+			},
+		}),
+	})
+
+	amplifyApp.addBranch(props.branch, {
+		stage: props.branch === 'main' ? 'PRODUCTION' : 'DEVELOPMENT',
+		branchName: props.branch,
+	})
+
+    amplify.Platform.WEB_COMPUTE
+
+	return amplifyApp
+}
--- lib/next_js-stack.ts
+++ lib/next_js-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { createAmplifyHosting } from './hosting/amplify';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NextJsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NextJsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const amplifyHostedApp = createAmplifyHosting(this, {
+      appName: "fullstack-amplify-example"
+      branch: "master"
+      ghOwner: "hariranjit"
+      repo: "hosting-fullstack-cdk"
+      ghTokenName: "github-token-ex"
+    })
+    
   }
 }
